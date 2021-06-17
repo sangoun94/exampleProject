b6e2a2fe7e483589bf1968e9290292a111c23ec5
sangoun94 <sangoun94@naver.com>
2021-06-17 09:33:12 +0900

    URL 클래스가 URLConnection 클래스 작업을 내부적으로 처리해준다

--- src/test/java/com/example/exampleproject/ExampleProjectApplicationTests.java
+++ src/test/java/com/example/exampleproject/ExampleProjectApplicationTests.java
@@ -1,13 +1,69 @@
 package com.example.exampleproject;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 @SpringBootTest
+@Slf4j
 class ExampleProjectApplicationTests {
 
+    URL ocu = null;
+    BufferedReader in = null;
+    String inputLine;
+    URLConnection conn = null;
     @Test
-    void contextLoads() {
+    void url() {
+        try {
+            ocu = new URL("https://naver.com");
+        } catch (MalformedURLException e) {
+            log.error(e.getMessage());
+        }
+        try {
+            in = new BufferedReader(new InputStreamReader(ocu.openStream()));
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+
+        try {
+            while (((inputLine = in.readLine()) != null))
+                System.out.println(inputLine);
+            in.close();
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    @Test
+    void urlConnection() {
+        try {
+            ocu = new URL("https://naver.com");
+            conn = ocu.openConnection();
+        } catch (MalformedURLException e) {
+            log.error(e.getMessage());
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+        try {
+            in = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+
+        try {
+            while (((inputLine = in.readLine()) != null))
+                System.out.println(inputLine);
+            in.close();
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
     }
 
 }
