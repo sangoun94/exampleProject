06ba9fc812f40ddb4419ff2ee96c1e597a7768c8
sangoun94 <sangoun94@naver.com>
2021-07-15 12:28:28 +0900

    K번째 수 테스트

--- src/main/java/com/example/exampleproject/programers/KakaoInternPpopggiEx.java
+++ src/main/java/com/example/exampleproject/programers/KakaoInternPpopggiEx.java
@@ -2,10 +2,10 @@ package com.example.exampleproject.programers;
 
 import java.util.Stack;
 
-public class kakaoInternPpopggiEx {
+public class KakaoInternPpopggiEx {
 
     public static void main(String[] args) {
-        kakaoInternPpopggiEx ex = new kakaoInternPpopggiEx();
+        KakaoInternPpopggiEx ex = new KakaoInternPpopggiEx();
         int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}};
         int[] moves = {1, 5, 3, 5, 1, 2, 1, 4};
 
