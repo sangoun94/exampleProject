27b193a56da4875abc1527793db3247eab4c1cb7
SangWoon-Hwang <sangoun94@naver.com>
2021-07-18 12:06:06 +0900

    잃어버린 체육복 LinkedList로 해결

--- src/main/java/com/example/exampleproject/programers/WorkoutClothes.java
+++ src/main/java/com/example/exampleproject/programers/WorkoutClothes.java
@@ -7,9 +7,10 @@ public class WorkoutClothes {
         WorkoutClothes workoutClothes = new WorkoutClothes();
         int n = 5;
         int[] lost = {2, 4};
-        int[] reserve = {1,3};
+        int[] reserve = {1, 2, 3};
         workoutClothes.solution(n, lost, reserve);
     }
+
     public int solution(int n, int[] lost, int[] reserve) {
         // 1. lost 와 reserve 인접해 있는지
         // 2. reserve 1명 구재
@@ -19,24 +20,31 @@ public class WorkoutClothes {
         LinkedList<Integer> integers = new LinkedList<>();
 
         arr = createArray(n, lost, reserve, arr);
-        integers = createLinkedList(arr, integers);
-        ListIterator<Integer> iterList = integers.listIterator();
+        integers = addLinkedList(arr, integers);
 
-        while (!iterList.hasNext()) {
-            if (iterList.next() != 2) {
+        integers = procList(integers);
 
-            }
+        for (int i = 0; i < integers.size(); i++) {
+            if (integers.get(i) > 0)
+                answer += 1;
         }
-
-
-
-
-
-
         return answer;
     }
 
-    private LinkedList<Integer> createLinkedList(int[] arr, LinkedList<Integer> integers) {
+    private LinkedList<Integer> procList(LinkedList<Integer> integers) {
+        for (int i = 0; i < integers.size(); i++) {
+            if (i-1 > -1 && integers.get(i - 1) == 0 && integers.get(i) == 2) {
+                integers.set(i - 1, 1);
+                integers.set(i, 1);
+            } else if (i+1 < integers.size() && integers.get(i + 1) == 0 && integers.get(i) == 2) {
+                integers.set(i + 1, 1);
+                integers.set(i, 1);
+            }
+        }
+        return integers;
+    }
+
+    private LinkedList<Integer> addLinkedList(int[] arr, LinkedList<Integer> integers) {
         for (int i : arr) {
             integers.add(i);
         }
@@ -50,10 +58,10 @@ public class WorkoutClothes {
             }
         }
         for (int i = 0; i < lost.length; i++) {
-            arr[lost[i]-1] = 0;
+            arr[lost[i] - 1] = arr[lost[i] - 1] - 1;
         }
         for (int i = 0; i < reserve.length; i++) {
-            arr[reserve[i]-1] = 2;
+            arr[reserve[i] - 1] = arr[reserve[i] - 1] + 1;
         }
         return arr;
     }
